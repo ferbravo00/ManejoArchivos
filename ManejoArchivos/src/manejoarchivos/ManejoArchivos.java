abc6480ff9ba9b47ea45bb1123bf7e065539a0ed
ferbravo00 <fer.bp001@gmail.com>
2022-10-11 10:26:02 +0200

    Update ManejoArchivos.java

--- ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -5,17 +5,34 @@
  */
 package manejoarchivos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Alumno Mañana
  */
 public class ManejoArchivos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    
+    public static void crear(String nombre) {
+        File archivo =new File(nombre);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+            
+        } catch (FileNotFoundException ex) {
+            
+            ex.printStackTrace(System.out);
+            
+        }
+                
     }
+    
+    
+    
     
 }
