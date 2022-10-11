e3a51b8b3fd55172acba584c1b4c2d5ac7f9e404
ferbravo00 <fer.bp001@gmail.com>
2022-10-11 10:25:51 +0200

    Create Principal.java

---
+++ ManejoArchivos/src/principal/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import manejoarchivos.*;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Principal {
+    
+    
+    public static void main(String[] args) {
+        String nombreArchivo = "prueba.txt";
+        ManejoArchivos.crear(nombreArchivo);
+        
+    }
+    
+}
