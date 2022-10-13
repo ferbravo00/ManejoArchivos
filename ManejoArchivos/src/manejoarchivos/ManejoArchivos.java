24a34f9a95ba849b6bb276c0904e2b57b3973591
ferbravo00 <fer.bp001@gmail.com>
2022-10-13 14:11:56 +0200

    ey

--- ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -5,9 +5,14 @@
  */
 package manejoarchivos;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,7 +37,69 @@ public class ManejoArchivos {
                 
     }
     
+    public static void escribir(String nombre,String contenido) {
+        File archivo =new File(nombre);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.println(contenido);
+            System.out.println("Se ha creado el fichero");
+            salida.close();
+            
+        } catch (FileNotFoundException ex) {
+            
+            ex.printStackTrace(System.out);
+            
+        }
+                
+    }
     
+    public static void agregar(String nombre,String contenido) {
+        File archivo =new File(nombre);
+        PrintWriter salida =null;
+        try {
+            salida = new PrintWriter(new FileWriter(nombre, true));
+            salida.println(contenido);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            salida.close();
+        }
+                
+    }
     
+    public static void leer(String nombre){
+        BufferedReader entrada = null;
+        File archivo =new File(nombre);
+        try {
+            entrada = new BufferedReader(new FileReader(archivo));
+            String lectura = entrada.readLine();
+            
+            while(lectura != null){
+                System.out.println("lectura = " + lectura);
+                lectura = entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void leerPorPalabra(String nombre){
+        File archivo =new File(nombre);
+        try {
+            Scanner entrada = new Scanner(archivo);
+            int cont=0;
+            while(entrada.hasNext()){
+                String palabra = entrada.next();
+                System.out.println(palabra);
+                cont++;
+            }
+            System.out.println("Numero de palabras = "+cont);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
     
 }
--- ManejoArchivos/src/principal/Principal.java
+++ ManejoArchivos/src/principal/Principal.java
@@ -15,9 +15,12 @@ public class Principal {
     
     
     public static void main(String[] args) {
-        String nombreArchivo = "prueba.txt";
+        String nombreArchivo = "prueba2.txt";
+        String contenido = "Hola esto es una prueva";
         ManejoArchivos.crear(nombreArchivo);
-        
+        ManejoArchivos.escribir(nombreArchivo, contenido);
+        ManejoArchivos.agregar(nombreArchivo, " para ver si funciona");
+        ManejoArchivos.leerPorPalabra(nombreArchivo);
     }
     
 }
